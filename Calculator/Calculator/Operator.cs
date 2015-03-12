339a0a2513916b137452bb6d6b266becf9f3520d
tarak1992 <tarak1992@gmail.com>
2015-03-12 09:17:48 -0400

    UPdated

--- Calculator/Calculator/Operator.cs
+++ Calculator/Calculator/Operator.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Calculator
 {
-    class Operator
+    public class BinaryOperation
+    {
+        public float PreviousTotal;
+        public string Operator = "+";
+        public float Operand;
+        public string StrOperand;
+        public float GetResults()
+        {
+            if (!float.TryParse(StrOperand, out Operand))
+            {
+                return PreviousTotal;       
+            }
+            switch (Operator)
+            {
+                case "+":
+                    return PreviousTotal + Operand;
+                case "-":
+                    return PreviousTotal - Operand;
+                case "x":
+                    return PreviousTotal * Operand;
+                case "/":
+                    return PreviousTotal / Operand;
+            }
+            return PreviousTotal;
+        }
+    }
+    public class Operations
     {
     }
 }
